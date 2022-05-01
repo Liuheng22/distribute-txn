606fee4297c8cb78efe6d7c278f7e630634b66ca
liuheng <liuheng19@126.com>
2022-05-02 00:32:42 +0800

    finish

---
+++ tinysql/store/tikv/dprintf.go
@@ -0,0 +1,29 @@
+package tikv
+
+import "log"
+
+// Debugging
+const fg = "all"
+const Debug = true
+
+func DPrintf(format string, a ...interface{}) (n int, err error) {
+	if Debug {
+		log.Printf(format, a...)
+	}
+	return
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+func min(a, b int) int {
+	if a > b {
+		return b
+	} else {
+		return a
+	}
+}
